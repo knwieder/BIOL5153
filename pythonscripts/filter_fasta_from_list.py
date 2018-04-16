acc54322be609883d164926c4fa09faee4275825
Katjana Wiederkehr <knwieder@uark.edu>
2018-04-16 16:20:22 -0500

    filter by list

--- pythonscripts/filter_fasta_from_list.py
+++ pythonscripts/filter_fasta_from_list.py
@@ -11,15 +11,33 @@ parser.add_argument("txt", help="name of text file - must be second", type=str)
 
 args = parser.parse_args()
 
-for files in SeqIO.parse(args.fasta, "fasta"):
-    with open(args.txt, 'r') as list:
-       names = [l.strip() for l in list]
+delete_these_seq = []
 
-    with open(args.fasta, 'r') as f:
-        for query in names:
-            for line in f:
-               if not query in line:
-                   print(line, next(f))
+with open(args.txt, 'r') as list:
+    for line in list.readlines():
+        delete_these_seq.append(line.strip())
+        #print(delete_these_seq)
+
+
+
+
+for files in SeqIO.parse(args.fasta, "fasta"):
+    if files.id not in delete_these_seq:
+        print(files.format("fasta"))
+
+    #    if files.id not in list:
+    #        print(files.id, '\n', files.seq)
+
+
+
+
+       #names = [l.strip() for l in list]
+
+    #with open(args.fasta, 'r') as f:
+    #    for query in names:
+        #    for line in f:
+        #       if not query in line:
+        #           print(line, next(f))
 
     #with open(args.fasta, 'r') as f:
     #    lines = [l.strip() for l in f]
