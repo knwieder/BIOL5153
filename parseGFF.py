79c564b5bf8405b9f58a0936b4d24a9b625d1384
Katjana Wiederkehr <knwieder@uark.edu>
2018-04-18 13:49:21 -0500

    parse functions

--- parseGFF.py
+++ parseGFF.py
@@ -4,30 +4,45 @@
 import argparse
 from Bio import SeqIO
 
-parser = argparse.ArgumentParser(description="This script filters out sequences from FASTA files")
+def get_args():
 
-parser.add_argument("fasta", help="name of FASTA file - must be first")
+    parser = argparse.ArgumentParser(description="This script filters out sequences from FASTA files")
 
-parser.add_argument("gff", help="name of GFF file - must be second")
+    parser.add_argument("fasta", help="name of FASTA file - must be first")
 
-args = parser.parse_args()
+    parser.add_argument("gff", help="name of GFF file - must be second")
 
-for genome in SeqIO.parse(args.fasta, "fasta"):
-    #print(genome.seq)
+return parser.parse_args()
 
-# get info from gff that tell which parts to print
-#open gff file and read in line by line with loop
-    with open(args.gff) as lines:
-        for line in lines.readlines():
-            (seqname, source, feature, start, end, score, strand, frame, attribute) = line.split("\t")
-            gene = line.split()[10]
-            print(">{}_{}".format(seqname, gene) + '\n' + (genome.seq[int(start):int(end)+1]))
+
+def rev_comp(dna):
+    dna = genone.seq
+    compA=(dna.replace("A", "t"))
+    compT=(compA.replace("T", "a"))
+    compC=(compT.replace("C", "g"))
+    compG=(compC.replace("G", "c"))
+
+    dna_comp = compG.upper()
+#print(dna_comp)
+    dna_comp = dna_comp[::-1]
+    print(dna_comp)
+return(dna_comp)
+
+def main():
+    for genome in SeqIO.parse(args.fasta, "fasta"):
+        dna = genome.seq
+
+        with open(args.gff) as lines:
+            for line in lines.readlines():
+                (seqname, source, feature, start, end, score, strand, frame, attribute) = line.split("\t")
+                gene = line.split()[10]
+                rev = line.split()[6]
+                if rev == "+"
+                    print(">{}_{}".format(seqname, gene) + '\n' + (genome.seq[int(start):int(end)+1]))
+                else
+                    print(">{}_{}".format(seqname, gene) + '\n' + (genome.seq.rev_comp()[int(start):int(end)+1]))
+
+args = get_args()
 # split line into list, use begin and end coords to extract info from the genome
 # print with header that tells gene and organism
 # close GFF file
-
-#find the headers for gff files
-#start = line.split()[4]
-#end = line.split()[5]
-#h1 = line.split()[0]
-#h2 = line.split()[1]
